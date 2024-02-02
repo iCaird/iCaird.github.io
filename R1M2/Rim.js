9e1991eda5baca8d27fccdda6da35bddfc9383a4
iCaird <42579723+iCaird@users.noreply.github.com>
2024-02-02 00:14:28 +0000

    add R1M2 files

---
+++ R1M2/Rim.js
@@ -0,0 +1,74 @@
+class Rim{
+  constructor(label,n,colour){
+    this.label = label;
+    this.n = n;
+    this.colour = colour;
+    
+    this.startPos = createVector(20,height/2);
+    this.points = [];
+    this.points[0] = this.startPos;
+    this.step = width/(this.n+3);
+    this.generatePoints();
+
+    this.clicked = false;
+    this.dragging = false;
+    this.offset = [];
+  }
+  
+  
+  show(){
+    noFill();
+    stroke(255,0,0);
+    strokeWeight(2);
+    beginShape();
+    for(point of this.points){
+      vertex(point.x,point.y);
+      push();
+      
+      fill(255,0,0);
+      circle(point.x,point.y,10);
+      pop();
+    }
+    endShape();
+  }
+  
+  update(){
+    if (this.dragging){
+      for(point of this.points){
+        point.set(mouseX + this.offset[this.points.indexOf(point)].x,mouseY + this.offset[this.points.indexOf(point)].y);
+      }
+    }
+  }
+  
+  generatePoints(){
+    for(i = 1; i < this.n+1; i++){
+      let x,y,nextPoint;
+      x = this.points[i-1].x + this.step;
+
+      if(this.label.includes(i)){
+        y = this.points[i-1].y + this.step;
+      } else {
+        y = this.points[i-1].y - this.step;
+      }
+      nextPoint = createVector(x,y);
+      this.points.push(nextPoint);
+    }
+  }
+
+  checkClicked(){
+    for(point of this.points){
+      if((point.x - mouseX)**2 + (point.y - mouseY)**2 <= 25){
+        this.clicked = !this.clicked;
+        this.dragging = true;
+        for(point of this.points){
+          this.offset.push(createVector(point.x-mouseX,point.y-mouseY));
+        }
+      }
+    }
+  }
+
+  released(){
+    this.dragging = false;
+  }
+}
+
---
+++ R1M2/sketch.js
@@ -0,0 +1,142 @@
+maxGrid = 12;
+n = 5;
+rims = [];
+
+function setup() {
+  createCanvas(400, 600);
+  setupGUI();
+}
+
+function draw() {
+  background(220);
+  drawGrid();
+  drawRims();
+  updateRims();
+  
+  
+}
+
+
+function setupGUI(){
+  labelEntry = createInput();
+  labelEntry.elt.focus();
+  labelEntry.id("labelEntry");
+  labelEntry.elt.focus();
+  
+  enterButton = createButton("Enter");
+  enterButton.mouseClicked(enter);
+  enterButton.id("enterButton");
+  
+  undoButton = createButton("Undo");
+  undoButton.mouseClicked(undo);
+  
+  
+  resetButton = createButton("Reset");
+  resetButton.mouseClicked(reset);
+  
+  nSelect = createSelect();
+  nSelect.size(40,21);
+  for(i = 1; i <= 20; i++){
+    nSelect.option(i);
+  }
+  nSelect.selected(n);
+  nSelect.changed(changeN);
+  
+  createP();
+  
+  trapButton = createButton("Trapeziums");
+  trapButton.mouseClicked(makeTrap);
+  
+  //maybe this should be a slider
+  
+  
+}
+
+function drawGrid(){
+  push();
+  strokeWeight(0);
+  fill(180);
+  for(i = 0; i <= n+2; i++){
+    for(j = 0; j <= n+2; j++){
+      circle(10 + i * 2 * width / (n + 3), 10 + j *2 * width / (n + 3),10);
+      circle(10 + (i+0.5)*2*width/(n+3),10 + (j+0.5)*2*width/(n+3),10);
+    }
+  }
+  pop();
+}
+
+
+
+function mousePressed(){
+  
+  
+}
+
+function mouseReleased(){
+  
+}
+
+function keyPressed(){
+  console.log(keyCode);
+  switch(keyCode) {
+      
+    case 13: //enter
+      enter();
+      break;
+    
+    default:
+      console.log("DEFAULT");
+      break;
+  }
+}
+
+function enter(){
+  //try {
+    label = labelEntry.value().split(" ").map(Number);
+    rim = new Rim(label,n,[255,0,0]);
+    rims.push(rim);
+  //}
+  //catch(err){
+     //errorMessage = createP("TRY AGAIN");
+  //}
+}
+
+function reset(){
+  
+}
+
+function makeTrap(){
+  
+}
+
+function undo(){
+  
+}
+
+function changeN(){
+  n = int(nSelect.value());
+}
+
+function drawRims(){
+  for(rim of rims){
+    rim.show();
+  }
+}
+
+function updateRims(){
+  for(rim of rims){
+    rim.update();
+  }
+}
+
+function mousePressed(){
+  for(rim of rims){
+    rim.checkClicked();
+  }
+}
+
+function mouseReleased(){
+  for(rim of rims){
+    rim.released();
+  }
+}
